4afde70807f1cb86babbccb3b84b38d762480d22
Aidin Gharibnavaz <aidin@aidinhut.com>
2018-01-19 20:17:21 +0330

    Now the project got a name!
    I added copyright notice, license file and a readme file.

---
+++ README.md
@@ -0,0 +1,10 @@
+Sillyverse
+----------
+
+Sillyverse is a collection of some programming game. In these games, the player writes programs and set them to live in a simulated environment. These software will compete over CPU time and/or Memory, the two requirements of a software to continue living.
+
+Name Origin
+-----------
+
+Sillyverse is how programs call their world. It is believed that the name is a combination of "silicon" and "universe" which misspelled. Some believe this was the original name.
+
--- emulator/src/hardware/operations.rs
+++ emulator/src/hardware/operations.rs
@@ -1,3 +1,19 @@
+// This file is part of Sillyverse.
+// Copyright (C) 2017, 2018, Aidin Gharibnavaz <aidin@aidinhut.com>
+//
+// Sillyverse is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// Sillyverse is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Sillyverse. If not, see <http://www.gnu.org/licenses/>.
+
 /// This module defines micro-operations of the CPU.
 
 use std::collections::HashMap;
